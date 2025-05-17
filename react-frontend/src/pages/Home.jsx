f5c3d7a56bd56ede728835e1021f5b831f136053
Naveenchand <naveenchand0606@gmail.com>
2025-05-17 21:03:02 +0530

    Loading fix

--- react-frontend/src/pages/Home.jsx
+++ react-frontend/src/pages/Home.jsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Home() {
+  const [isLoading, setLoading] = useState(false);
   const [data1, setData1] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     axios.get(`${import.meta.env.VITE_API_BASE_URL}/data1`)
       .then(res => setData1(res.data))
       .catch(err => console.error(err));
+    setLoading(false);
   }, []);
 
   return (
     <div>
       <h2>Home Page</h2>
-      <pre>{JSON.stringify(data1, null, 2)}</pre>
+      {isLoading ? "Loading..." :  <pre>{JSON.stringify(data1, null, 2)}</pre>}
     </div>
   );
 }
